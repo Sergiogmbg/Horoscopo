d876feac9c8a802010d151d55e850e0476b6176c
Sergio <sergiog.mbg@gmail.com>
2020-02-26 10:49:16 +0100

    Horóscopo terminado.

--- src/java/modelo/Utilidades.java
+++ src/java/modelo/Utilidades.java
@@ -50,9 +50,65 @@ public class Utilidades {
                 break;
             case 4:
                 if (dia > 20) {
-                    signo = "Acuario";
+                    signo = "Tauro";
                 } else {
+                    signo = "Aries";
+                }
+                break;
+            case 5:
+                if (dia > 20) {
+                    signo = "Géminis";
+                } else {
+                    signo = "Tauro";
+                }
+                break;
+            case 6:
+                if (dia > 20) {
+                    signo = "Cáncer";
+                } else {
+                    signo = "Géminis";
+                }
+                break;
+            case 7:
+                if (dia > 20) {
+                    signo = "Leo";
+                } else {
+                    signo = "Cáncer";
+                }
+                break;
+            case 8:
+                if (dia > 20) {
+                    signo = "Virgo";
+                } else {
+                    signo = "Leo";
+                }
+                break;
+            case 9:
+                if (dia > 20) {
+                    signo = "Libra";
+                } else {
+                    signo = "Virgo";
+                }
+                break;
+            case 10:
+                if (dia > 20) {
+                    signo = "Escorpio";
+                } else {
+                    signo = "Libra";
+                }
+                break;
+            case 11:
+                if (dia > 20) {
+                    signo = "Sagitario";
+                } else {
+                    signo = "Escorpio";
+                }
+                break;
+            case 12:
+                if (dia > 20) {
                     signo = "Capricornio";
+                } else {
+                    signo = "Sagitario";
                 }
                 break;
         }
